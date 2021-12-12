f1d4d91a1fdebb5217cc5d724468c88031184abf
Sean Yeo <seanyeo89@gmail.com>
2021-12-12 19:15:30 +0800

    Attempt at stock data instead of covid due to access issues

--- src/app.py
+++ src/app.py
@@ -10,8 +10,43 @@ from dash.dependencies import Input, Output
 app = dash.Dash(__name__)
 server = app.server
 
+df = px.data.stocks()
+
+
+app.layout = html.Div(id = 'parent', children = [
+    html.H1(id = 'H1', children = 'Styling using html components', style = {'textAlign':'center',\
+                                            'marginTop':40,'marginBottom':40}),
+
+        dcc.Dropdown( id = 'dropdown',
+        options = [
+            {'label':'Google', 'value':'GOOG' },
+            {'label': 'Apple', 'value':'AAPL'},
+            {'label': 'Amazon', 'value':'AMZN'},
+            ],
+        value = 'GOOG'),
+        dcc.Graph(id = 'bar_plot')
+    ])
+    
+    
+@app.callback(Output(component_id='bar_plot', component_property= 'figure'),
+              [Input(component_id='dropdown', component_property= 'value')])
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    fig = go.Figure([go.Scatter(x = df['date'], y = df['{}'.format(dropdown_value)],\
+                     line = dict(color = 'firebrick', width = 4))
+                     ])
+    
+    fig.update_layout(title = 'Stock prices over time',
+                      xaxis_title = 'Dates',
+                      yaxis_title = 'Prices'
+                      )
+    return fig  
+
+
 #---------------------------------------------------------------
 #Taken from https://www.ecdc.europa.eu/en/geographical-distribution-2019-ncov-cases
+
+"""
 df = pd.read_csv("Dec2020data.csv")
 
 dff = df.groupby('countriesAndTerritories', as_index=False)[['deaths','cases']].sum()
@@ -134,6 +169,7 @@ def update_data(chosen_rows,piedropval,linedropval):
     line_chart.update_layout(uirevision='foo')
 
     return (pie_chart,line_chart)
+"""
 
 @app.route("/greeting")
 def greet():

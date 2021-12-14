807a3851d082c11eaa673075bd0eb0bda278c71c
Sean Yeo <seanyeo89@gmail.com>
2021-12-14 18:32:06 +0800

    Set up a pytest for random range selection to decide whether or not to work that day.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,37 +1,45 @@
-import time
- 
-# Functions are the first class objects in Python. 
-# What it means is that they can be treated just like other variables and you can pass them as
-# arguments to another function  or even return them as a return value.
- 
-def time_it (func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print(func.__name__ + "took" + str(1000*(end - start)) + "mil sec")
-        return result
-    return wrapper
- 
- 
-@time_it
-def calc_square(num):
-    start = time.time()
-    result = []
-    for num in num:
-        result.append(num*num)
-        end = time.time()
-        print("calc_square took: " + str(1000*(end - start)) + "mil sec")
- 
- 
-@time_it
-def calc_cude(num):
-    start = time.time()
-    result = []
-    for num in num:
-        result.append(num*num*num)
-        end = time.time()
-        print("calc_cube took: " + str(1000*(end-start)) + "mil sec")
- 
-array = range(1,100)
-out_square = calc_square(array)
+# DATABASE
+# Columns: | Player Id | Player Name | Team Name | Age | Average Score | 
+from random import randrange
+Database={
+    1:["FB", "A", 23, 58],
+    2:["AMZN", "B", 22, 60],
+    3:["AAPL", "C", 23, 56],
+    4:["TSLA", "D", 24, 53]
+        }
+
+
+
+# API simulation of READ operation
+
+# Input Payload
+'''
+    payload={
+            "Player_id": id
+        }
+'''
+
+def read_player(payload):
+    id = payload["Player_id"]
+    if id in Database:
+        entry=Database[id]
+        return {"Code":200, 
+                "Response":{"Name":entry[0],"Team":entry[1],"Age":entry[2],"Average":entry[3]},
+                "Message":"Read Operation Succesful"}
+
+    else:
+        return {"Code":404,"Message":"Player does not exists"}
+
+
+
+# API Testing Function
+# Code deployment will fail some time due to random range theoratically.
+# Testing function
+
+test_payload={
+        "Player_id": randrange(5)
+    }
+
+def test_read_player():
+    response=read_player(test_payload)
+    assert response.get("Code")==200

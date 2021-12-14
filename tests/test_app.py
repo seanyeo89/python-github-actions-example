121081ec44894aa2c6b12b16b963045a0596fc67
Sean Yeo <seanyeo89@gmail.com>
2021-12-14 17:33:28 +0800

    attempt at test app function edit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,37 @@
-def inc(x):
-    return x + 1
-
-
-def test_answer():
-    assert inc(4) == 5
+import time
+ 
+# Functions are the first class objects in Python. 
+# What it means is that they can be treated just like other variables and you can pass them as
+# arguments to another function  or even return them as a return value.
+ 
+def time_it (func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name___ + "took" + str((end - start) * 1000 + "mil sec"))
+        return result
+    return wrapper
+ 
+ 
+@time_it
+def calc_square(num):
+    start = time.time()
+    result = []
+    for num in num:
+        result.append(num*num)
+        end = time.time()
+        print("calc_square took: " + str((end - start) * 1000 + "mil sec"))
+ 
+ 
+@time_it
+def calc_cude(num):
+    start = time.time()
+    result = []
+    for num in num:
+        result.append(num*num*num)
+        end = time.time()
+        print("calc_cube took: " + str((end-start)*1000 + "mil sec"))
+ 
+array = range(1,100000)
+out_square = calc_square(array)
